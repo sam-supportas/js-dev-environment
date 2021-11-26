ebf36be6b43d7afd79b5daa748f5bb7a445124d4
Jeremy Brown <jeremy.brown@proofgeist.com>
2021-11-26 07:49:19 -0500

    updated, removed branding and custom dialogs

--- public/index.js
+++ public/index.js
@@ -1,4 +1,11 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+//Here we're importing items we'll need. You can add other imports here.
 import "./style.css";
-import "regenerator-runtime/runtime.js";
+// import "regenerator-runtime/runtime.js";
 
+//The first function. Remove this.
+const btn = document.querySelector("button");
+btn.onclick = function () {
+  alert("You ran some JavaScript");
+};
+
+//
